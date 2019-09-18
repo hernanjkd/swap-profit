467225cdaa0b6a7a312f1a000981bf0d2a322821
hernanjkd <hernanjkd@gmail.com>
2019-09-18 17:54:11 -0400

    dummy data

--- src/main.py
+++ src/main.py
@@ -26,28 +26,85 @@ def handle_invalid_usage(error):
 
 flights = [
     {
-        "id": 54,
-        "start_at": "Wed, 11 Oct 2019 12:00:00 GMT",
-        "end_at": "Wed, 11 Oct 2019 16:00:00 GMT",
-        "tournament_id": 89,
+        "id": 1,
+        "start_at": "Wed, 11 Oct 2019 16:00:00 GMT",
+        "end_at": "Wed, 11 Oct 2019 21:00:00 GMT",
+        "tournament_id": 1,
         "created_at": "Mon, 16 Sep 2019, 14:55:32",
         "updated_at": "Tue, 17 Sep 2019, 22:44:07",
         "buy_ins": []
     },
     {
-        "id": 55,
-        "start_at": "Wed, 11 Oct 2019 12:00:00 GMT",
-        "end_at": "Wed, 11 Oct 2019 16:00:00 GMT",
-        "tournament_id": 89,
+        "id": 2,
+        "start_at": "Wed, 11 Oct 2019 16:00:00 GMT",
+        "end_at": "Wed, 11 Oct 2019 21:00:00 GMT",
+        "tournament_id": 1,
         "created_at": "Mon, 16 Sep 2019, 14:55:32",
         "updated_at": "Tue, 17 Sep 2019, 22:44:07",
         "buy_ins": []
-    }
+    },
+    {
+        "id": 3,
+        "start_at": "Mon, 30 Sep 2019 12:00:00 GMT",
+        "end_at": "Mon, 30 Sep 2019 21:00:00 GMT",
+        "tournament_id": 2,
+        "created_at": "Mon, 16 Sep 2019, 14:55:32",
+        "updated_at": "Tue, 17 Sep 2019, 22:44:07",
+        "buy_ins": []
+    },
+    {
+        "id": 4,
+        "start_at": "Tue, 1 Oct 2019 12:00:00 GMT",
+        "end_at": "Tue, 1 Oct 2019 21:00:00 GMT",
+        "tournament_id": 2,
+        "created_at": "Mon, 16 Sep 2019, 14:55:32",
+        "updated_at": "Tue, 17 Sep 2019, 22:44:07",
+        "buy_ins": []
+    },
+    {
+        "id": 5,
+        "start_at": "Wed, 11 Oct 2019 12:00:00 GMT",
+        "end_at": "Wed, 11 Oct 2019 16:00:00 GMT",
+        "tournament_id": 3,
+        "created_at": "Mon, 16 Sep 2019, 14:55:32",
+        "updated_at": "Tue, 17 Sep 2019, 22:44:07",
+        "buy_ins": []
+    },
 ]
 
-users = [
+profiles = [
     {
-        "id": 213,
+        "id": 1,
+        "first_name": "Cary",
+        "last_name": "Katz",
+        "email": "katz234@gmail.com",
+        "hendon_url": "https://pokerdb.thehendonmob.com/player.php?a=r&n=26721",
+        "hendon_username": "Cary Katz",
+        "profile_picture_url": "https://pokerdb.thehendonmob.com/pictures/carykatzpic.png",
+        "transactions": "list of transactions",
+        "created_at": "Tue, 17 Sep 2019 04:23:59 GMT",
+        "updated_at": "Tue, 17 Sep 2019 04:23:59 GMT",
+        "tokens": 12,
+        "swaps": [],
+        "buy_ins": []
+    },
+    {
+        "id": 2,
+        "first_name": "Kate",
+        "last_name": "Hoang",
+        "email": "hoang234@gmail.com",
+        "hendon_url": "https://pokerdb.thehendonmob.com/player.php?a=r&n=421758",
+        "hendon_username": "Kate Hoang",
+        "profile_picture_url": "https://pokerdb.thehendonmob.com/pictures/Hoang_2.jpg",
+        "transactions": "list of transactions",
+        "created_at": "Tue, 17 Sep 2019 04:23:59 GMT",
+        "updated_at": "Tue, 17 Sep 2019 04:23:59 GMT",
+        "tokens": 12,
+        "swaps": [],
+        "buy_ins": []
+    },
+    {
+        "id": 3,
         "first_name": "Cary",
         "last_name": "Katz",
         "email": "katz234@gmail.com",
@@ -65,7 +122,7 @@ users = [
 
 tournaments = [
     {
-        "id": 89,
+        "id": 1,
         "name": "Heartland Poker Tour - HPT Colorado, Black Hawk",
         "address": "261 Main St, Black Hawk, CO 80422",
         "start_at": "Wed, 11 Oct 2019 12:00:00 GMT",
@@ -73,18 +130,40 @@ tournaments = [
         "created_at": "Mon, 16 Sep 2019, 14:55:32",
         "updated_at": "Tue, 17 Sep 2019, 22:44:07",
         "swaps": "should we put a list of all the swaps for the tournament?",
-        "flights": list(filter(lambda x: x["tournament_id"] == 89, flights))
+        "flights": list(filter(lambda x: x['tournament_id'] == 1, flights))
+    },
+    {
+        "id": 2,
+        "name": "Stones Live Fall Poker Series",
+        "address": "6510 Antelope Rd, Citrus Heights, CA 95621",
+        "start_at": "Mon, 30 Sep 2019 11:00:00 GMT",
+        "end_at": "Tue, 1 Oct 2019 22:00:00 GMT",
+        "created_at": "Mon, 16 Sep 2019, 14:55:32",
+        "updated_at": "Tue, 17 Sep 2019, 22:44:07",
+        "swaps": "should we put a list of all the swaps for the tournament?",
+        "flights": list(filter(lambda x: x['tournament_id'] == 2, flights))
+    },
+    {
+        "id": 3,
+        "name": "WPT DeepStacks - WPTDS Sacramento",
+        "address": "Thunder Valley Casino Resort, 1200 Athens Ave, Lincoln, CA 95648",
+        "start_at": "Wed, 2 Oct 2019 12:00:00 GMT",
+        "end_at": "Wed, 2 Oct 2019 21:00:00 GMT",
+        "created_at": "Mon, 16 Sep 2019, 14:55:32",
+        "updated_at": "Tue, 17 Sep 2019, 22:44:07",
+        "swaps": "should we put a list of all the swaps for the tournament?",
+        "flights": list(filter(lambda x: x['tournament_id'] == 3, flights))
     }
 ]
 
 
-@app.route('/tournaments', methods=['GET'])
-def get_tournament():
-    return jsonify(tournaments)
+@app.route('/tournament/<int:id>', methods=['GET'])
+def get_tournament(id):
+    return jsonify(list(filter(lambda x: x['id'] == id, tournaments))[0])
 
-@app.route('/profile/<int:id>')
+@app.route('/profile/<int:id>', methods=['GET'])
 def get_profile(id):
-    return jsonify()
+    return jsonify(list(filter(lambda x: x['id'] == id, profiles))[0])
 
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
