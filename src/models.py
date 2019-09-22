fe4dd1962d8c785b038d8a67ed531af88b13376f
hernanjkd <hernanjkd@gmail.com>
2019-09-22 03:51:00 -0400

    tables

--- src/models.py
+++ src/models.py
@@ -32,11 +32,12 @@ class Profiles(db.Model):
     last_name = db.Column(db.String(100), nullable=False)
     username = db.Column(db.String(100))
     hendon_url = db.Column(db.String(200))
-    profile_picture_url = db.Column(db.String(200))
+    profile_picture_url = db.Column(db.String(250))
 
     user = db.relationship('Users', back_populates='profile', uselist=False)
-    swaps = db.relationship('Swaps', back_populates='user')
     buy_ins = db.relationship('Buy_ins', back_populates='players')
+    # sending_swaps = db.relationship('Swaps', back_populates='sender_user')
+    # recieving_swaps = db.relationship('Swaps', back_populates='recipient_user)
 
     def __repr__(self):
         return f'<Profiles {self.first_name} {self.last_name}>'
@@ -51,15 +52,14 @@ class Profiles(db.Model):
             "profile_picture_url": self.profile_picture_url
         }
         if long:
-            json = {
+            return {
                 **json,
                 "hendon_url": self.hendon_url,
                 "created_at": "",
                 "updated_at": "",
-                "swaps": list(map(lambda x: x.serialize(), self.swaps)),
+                "swaps": list(map(lambda x: x.serialize(), self.recieving_swaps)),
                 "buy_ins": list(map(lambda x: x.serialize(), self.buy_ins))
             }
-            
         return json
 
 
@@ -121,47 +121,55 @@ class Flights(db.Model):
 class Swaps(db.Model):
     __tablename__ = 'swaps'
     tournament_id = db.Column(db.Integer, db.ForeignKey('tournaments.id'), primary_key=True)
-    sender_id = db.Column(db.Integer, db.ForeignKey('users.id'), primary_key=True)
-    recipient_id = db.Column(db.Integer, primary_key=True)
+    recipient_id = db.Column(db.Integer, db.ForeignKey('profiles.id'), primary_key=True)
+    sender_id = db.Column(db.Integer, db.ForeignKey('profiles.id') primary_key=True)
     percentage = db.Column(db.Integer, nullable=False)
     winning_chips = db.Column(db.Integer, default=None)
     due_at = db.Column(db.DateTime, default=None)
 
     tournament = db.relationship('Tournaments', back_populates='swaps')
-    user = db.relationship('Profiles', back_populates='swaps')
+    sender_user = db.relationship('Profiles', foreign_keys=[sender_id], backref='sending_swaps')
+    recipient_user = db.relationship('Profiles', foreign_keys=[recipient_id], backref='reccieving_swaps')
 
     def __repr__(self):
         return f'<Swaps {self.user.email} {self.recipient_id} {self.tournament.name}>'
 
     def serialize(self, long=False):
         json = {
-
-        }
-        return {
             "tournament_id": self.tournament_id,
-            "sender_id": self.sender_id,
-            "recipient_id": self.recipient_id,
+            "recipient_user": self.recipient_user.serialize(),
             "percentage": self.percentage,
             "winning_chips": self.winning_chips,
             "due_at": self.due_at,
-            "created_at": "",
-            "updated_at": ""
         }
+        if long:
+            return {
+                **json,
+                "sender_id": self.sender_id,
+                "created_at": "",
+                "updated_at": ""
+            }
+        return json
 
 
 class Transactions(db.Model):
     __tablename__ = 'transactions'
     id = db.Column(db.Integer, primary_key=True)
-#     amount = db.Column(db.Integer)
-
-#     user_id = db.Column(db.Integer, db.ForeignKey('Users.id'))
-
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    amount_in_coins = db.Column(db.Integer)
+    amount_in_dollars = db.Column(db.Integer)
 
 class Buy_ins(db.Model):
     __tablename__ = 'buy_ins'
     id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('profiles.id'))
+    flight_id = db.Column(db.Integer, db.ForeignKey('flights.id'))
+    receipt_image_url = db.Column(db.String(250))
 
 class Tokens(db.Model):
     __tablename__ = 'tokens'
     id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    token = db.Column(db.String(500))
+    expires_at = db.Column(db.DateTime)
 
