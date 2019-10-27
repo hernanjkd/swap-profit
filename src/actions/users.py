e854426ee32f1172f34a3c896c5fb45746b0bbd8
hernanjkd <hernanjkd@gmail.com>
2019-10-26 23:44:37 -0400

    testing

--- src/actions/users.py
+++ src/actions/users.py
@@ -14,3 +14,13 @@ from datetime import datetime, timedelta
 
 def update_user(data, user):
 
+
+from flask_jwt_simple import JWTManager, jwt_required, create_jwt, decode_jwt, get_jwt
+from utils import APIException, generate_sitemap, check_params, validation_link, update_table, sha256
+from dummy_data import buy_ins, flights, swaps, profiles, tournaments
+from models import db, Users, Profiles, Tournaments, Swaps, Flights, Buy_ins, Transactions, Tokens
+from datetime import datetime, timedelta
+
+
+
+def update_user(data, user):
