b00f6177ac2302dd9924c17a50605aa02c17457c
hernanjkd <hernanjkd@gmail.com>
2019-12-10 20:23:13 -0500

    excluding created_at and updated_at fields from admin

--- src/admin.py
+++ src/admin.py
@@ -1,20 +1,24 @@
 from flask_admin.contrib.sqla import ModelView
 from flask_admin import Admin
 from models import (db, Users, Profiles, Tournaments, Flights, Swaps, Buy_ins,
-    Transactions, Tournaments, Coins)
-# Flask and Flask-SQLAlchemy initialization here
+    Transactions, Tournaments, Coins, Devices)
+
 
 def SetupAdmin(app):
+
+    class ExcludedModelView(ModelView):
+        form_excluded_columns = ['created_at', 'updated_at']
+    
     admin = Admin(app, name='Swap Profit', template_mode='bootstrap3')
-    admin.add_view(ModelView(Users, db.session))
-    admin.add_view(ModelView(Profiles, db.session))
-    admin.add_view(ModelView(Tournaments, db.session))
-    admin.add_view(ModelView(Flights, db.session))
-    admin.add_view(ModelView(Swaps, db.session))
-    admin.add_view(ModelView(Buy_ins, db.session))
-    admin.add_view(ModelView(Transactions, db.session))
-    admin.add_view(ModelView(Coins, db.session))
 
-
+    admin.add_view(ExcludedModelView(Users, db.session))
+    admin.add_view(ExcludedModelView(Profiles, db.session))
+    admin.add_view(ExcludedModelView(Tournaments, db.session))
+    admin.add_view(ExcludedModelView(Flights, db.session))
+    admin.add_view(ExcludedModelView(Swaps, db.session))
+    admin.add_view(ExcludedModelView(Buy_ins, db.session))
+    admin.add_view(ExcludedModelView(Transactions, db.session))
+    admin.add_view(ExcludedModelView(Coins, db.session))
+    admin.add_view(ExcludedModelView(Devices, db.session))
 
     return admin
