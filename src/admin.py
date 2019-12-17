4fd98af6a5ec28949ea5b5f79395f9ccccd9cef0
hernanjkd <hernanjkd@gmail.com>
2019-12-17 01:00:51 -0500

    fixes

--- src/admin.py
+++ src/admin.py
@@ -1,7 +1,7 @@
 from flask_admin.contrib.sqla import ModelView
 from flask_admin import Admin
 from models import (db, Users, Profiles, Tournaments, Flights, Swaps, Buy_ins,
-    Transactions, Tournaments, Devices)
+    Transactions, Devices)
 
 
 def SetupAdmin(app):
@@ -11,13 +11,13 @@ def SetupAdmin(app):
     
     admin = Admin(app, name='Swap Profit', template_mode='bootstrap3')
 
-    admin.add_view(ExcludedModelView(Users, db.session))
-    admin.add_view(ExcludedModelView(Profiles, db.session))
-    admin.add_view(ExcludedModelView(Tournaments, db.session))
-    admin.add_view(ExcludedModelView(Flights, db.session))
-    admin.add_view(ExcludedModelView(Swaps, db.session))
-    admin.add_view(ExcludedModelView(Buy_ins, db.session))
-    admin.add_view(ExcludedModelView(Transactions, db.session))
-    admin.add_view(ExcludedModelView(Devices, db.session))
+    admin.add_view( ExcludedModelView( Users, db.session ))
+    admin.add_view( ExcludedModelView( Profiles, db.session ))
+    admin.add_view( ExcludedModelView( Tournaments, db.session ))
+    admin.add_view( ExcludedModelView( Flights, db.session ))
+    admin.add_view( ExcludedModelView( Swaps, db.session ))
+    admin.add_view( ExcludedModelView( Buy_ins, db.session ))
+    admin.add_view( ExcludedModelView( Transactions, db.session ))
+    admin.add_view( ExcludedModelView( Devices, db.session ))
 
     return admin
