a904cf91974b03cbb23fb3de1a76ff60dfaeceaa
Hernán García <hernanjkd@gmail.com>
2019-12-23 19:13:49 -0500

    Update notifications.py

--- src/notifications.py
+++ src/notifications.py
@@ -31,8 +31,8 @@ def send_email(type, to, data={}):
                 'text': template['text'],
                 'html': template['html']
             })
-        print(r.content)
-        return r.status_code == 200
+        
+        return r
         
     return False
 
