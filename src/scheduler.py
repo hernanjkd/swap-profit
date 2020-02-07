7d962d9298aad8e1b925f06c7133191887cf6367
hernanjkd <hernanjkd@gmail.com>
2020-02-06 21:40:18 -0500

    trying code in scheduler

--- src/scheduler.py
+++ src/scheduler.py
@@ -5,54 +5,26 @@ from models import Tournaments, Flights
 from datetime import datetime, timedelta
 
 
-engine = create_engine('postgres://Francine@localhost/swapprofit')
-Session = sessionmaker(bind=engine)
+engine = create_engine( os.environ.get('DATABASE_URL'))
+Session = sessionmaker( bind=engine )
 session = Session()
 
-country = Tournaments(
-    name='Coconut Creek - NLH Survivor $2,000 Guaranteed',
-    address='287 Carrizo Canyon Rd',
-    city='Mescalero',
-    state='NM',
-    zip_code='12084',
-    latitude=33.2956,
-    longitude=-105.6901,
-    start_at=datetime(2018,12,29,10)
-)
-flight1_country = Flights(
-    start_at=datetime(2018,12,29,10),
-    tournament= country
-)
-flight2_country = Flights(
-    start_at=datetime(2020,12,29,10),
-    tournament= country
-)
-# db.session.add_all([country, flight1_country])
+time = datetime.utcnow() + timedelta(days=1)
+trmnt = session.query(Tournaments) \
+    .filter( func.max(Tournaments.id) )
+x = [z for z in trmnt]
+print(x)
+# trmnts = session.query(Tournaments) \
+    # .filter_by(name='New Vegas Strip - Texas Hold\'em Finale') \
+    # .filter( Tournaments.flights.any(
+    #     func.max(Flights.start_at) < time
+    # ))
 
-_17hrs_ago = datetime.utcnow() - timedelta(hours=17)
-trmnts = session.query(Tournaments) \
-            .filter( Tournaments.status == 'waiting_results')
-            # .filter( Tournaments.flights.any( 
-            #     func.max(Flights.start_at) < _17hrs_ago ))
+
+# _17hrs_ago = datetime.utcnow() - timedelta(hours=17)
+# trmnts = session.query(Tournaments) \
+#             .filter( Tournaments.status == 'open')
+#             .where( Tournaments.flights.any( 
+#                 func.max(Flights.start_at) < _17hrs_ago ))
             # .filter( Tournaments.flights)
-
-for t in [x for x in trmnts]:
-    print(t.status._value_)
-
-
-
-# session = Session.object_session(Users)
-# users = Session.query(Users).get(1)
-
-
-
-# metadata = MetaData()
-# connection = engine.connect()
-
-# users = Table('users', metadata, autoload=True, autoload_with=engine)
-# print( repr(users) )
-
-
-
-
 
