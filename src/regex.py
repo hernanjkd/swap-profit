e190b24576e4994c28387c7e73bd9b79e113145b
hernanjkd <hernanjkd@gmail.com>
2020-03-12 06:30:03 -0400

    hard rock regex updated

--- src/regex.py
+++ src/regex.py
@@ -3,22 +3,24 @@ import re
 def hard_rock(text):
     buyin = re.search(r'buy[\s\-_]*in\D{1,5}([0-9,\.]+)', text, re.IGNORECASE)
     buyin = buyin and buyin.group(1)
-    seat = re.search(r'seat\D{,5}\d+\D+([0-9]+)', text, re.IGNORECASE)
+    seat = re.search(r'seat\D+\d+\D+(\d+)', text, re.IGNORECASE)
     seat = seat and seat.group(1)
-    table = re.search(r'seat\D{,5}([0-9]+)', text, re.IGNORECASE)
+    table = re.search(r'seat\D+(\d+)', text, re.IGNORECASE)
     table = table and table.group(1)
     name = re.search(r'name[ :,]+([a-zA-Z() ]+)', text, re.IGNORECASE)
     name = name and name.group(1)
-    date = re.search(r'received\w+(\w+ \d+, \d+ \d+:\d \w\w)' ,text, re.IGNORECASE)
-    date = re.search(r'(\w+ \d+, \d+ \d+:\d+ \w\w)' ,text, re.IGNORECASE)
+    date = re.search(r'received[ :\w]+\n(.*)\n',text, re.IGNORECASE)
     date = date and date.group(1)
-    id = re.search(r'player[a-z :]+(\d+)', text, re.IGNORECASE)
+    id = re.search(r'player\D+(\d+)', text, re.IGNORECASE)
     id = id and id.group(1)
+    cas = re.search(r'^([^\n]+)\n([^\n]+)', text, re.IGNORECASE)
+    casino = cas and f'{cas.group(1)} {cas.group(2)}'
     return {
-        'player_name': name,
-        'date_on_receipt': date,
-        'buyin_amount': buyin,
+        'casino': casino,
+        'player name': name,
+        'player id': id,
+        'buyin amount': buyin,
         'seat': seat,
         'table': table,
-        'player_id': id
+        'receipt timestamp': date
     }
